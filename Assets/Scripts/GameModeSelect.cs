5dd17ba7679a3a81e2373419389dfa7d44d6b939
Evan David McClenaghan Finnigan <efinnig@uwo.ca>
2018-03-24 15:39:20 -0400

    Logic for select screen

---
+++ Assets/Scripts/GameModeSelect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameModeSelect : MonoBehaviour
+{
+
+    public GameObject instructionCanvas;
+    public GameObject mainCanvas;
+
+    public void BtnPractice()
+    {
+        // Practice button logic
+        SceneManager.LoadScene("Practice");
+    }
+
+    public void BtnMatch()
+    {
+        // Match Game button Logic
+        SceneManager.LoadScene("Match");
+    }
+
+    public void BtnEqual()
+    {
+        // Are We Equal button logic
+        SceneManager.LoadScene("Are We Equal");
+    }
+
+    public void BtnInstructions()
+    {
+        // Instructions button logic
+        instructionCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
+    }
+
+    public void BtnBack()
+    {
+        // Back from instructions
+        mainCanvas.SetActive(true);
+        instructionCanvas.SetActive(false);
+    }
+
+}
