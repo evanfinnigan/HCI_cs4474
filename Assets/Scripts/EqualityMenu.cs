86cb10c3d6beaab3cbb4c954a82e6e5f81c3b643
Evan David McClenaghan Finnigan <efinnig@uwo.ca>
2018-03-24 16:00:31 -0400

    Equality mode menu

---
+++ Assets/Scripts/EqualityMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EqualityMenu : MonoBehaviour {
+
+    public GameObject instructionCanvas;
+    public GameObject mainCanvas;
+
+    public void BtnPlay()
+    {
+        // Practice button logic
+        SceneManager.LoadScene("Are We Equal");
+    }
+
+    public void BtnReturnToGameSelect()
+    {
+        // Match Game button Logic
+        SceneManager.LoadScene("Game Mode Select");
+    }
+
+    public void BtnInstructions()
+    {
+        // Instructions button logic
+        instructionCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
+    }
+
+    public void BtnBack()
+    {
+        // Back from instructions
+        mainCanvas.SetActive(true);
+        instructionCanvas.SetActive(false);
+    }
+
+}
