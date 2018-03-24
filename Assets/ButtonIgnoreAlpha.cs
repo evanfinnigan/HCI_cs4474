599c520a3a6097e3a8a5bc786cea62c37b815c84
Evan David McClenaghan Finnigan <efinnig@uwo.ca>
2018-03-23 20:27:58 -0400

    Initial scenes with UI stuff, no functionality yet

---
+++ Assets/ButtonIgnoreAlpha.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonIgnoreAlpha : MonoBehaviour {
+
+    public Image buttonImage;
+
+    private void Awake()
+    {
+        if (buttonImage != null)
+        {
+            buttonImage.alphaHitTestMinimumThreshold = 0.1f;
+        }
+    }
+}
