2984df22e1e49e7f8800865034133d8111536082
Evan David McClenaghan Finnigan <efinnig@uwo.ca>
2018-04-03 17:11:28 -0400

    Initial equality mode. much like practice, right now it just does debug.log instead of giving user feedback. Also, it is infinite, there are no points or end or anything.

---
+++ Assets/Scripts/EqualityMode.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EqualityMode : MonoBehaviour {
+
+    public Image fractionImage;
+    public Text fractionText;
+
+    public int maxNumerator;
+    public int maxDenomenator;
+
+    int pizzaNumerator;
+
+    int numerator;
+    int denomenator;
+
+	// Use this for initialization
+	void Start () {
+        GenerateFraction();
+	}
+	
+    public void GenerateFraction()
+    {
+        denomenator = Random.Range(1, maxDenomenator);
+        numerator = Random.Range(1, maxNumerator > denomenator ? denomenator : maxNumerator);
+
+        if (Random.Range(0f,1f) > 0.5f)
+        {
+            pizzaNumerator = numerator;
+        }
+        else
+        {
+            Random.Range(1, maxNumerator > denomenator ? denomenator : maxNumerator);
+        }
+
+        fractionText.text = "" + numerator + "/" + denomenator;
+        fractionImage.fillAmount = ((float)pizzaNumerator) / denomenator;
+    }
+
+	public void BtnYes()
+    {
+        if (pizzaNumerator == numerator)
+        {
+            Debug.Log("Correct!");
+            GenerateFraction();
+        }
+        else
+        {
+            Debug.Log("Incorrect! Try Again!");
+        }
+    }
+
+    public void BtnNo()
+    {
+        if (pizzaNumerator != numerator)
+        {
+            Debug.Log("Correct!");
+            GenerateFraction();
+        }
+        else
+        {
+            Debug.Log("Incorrect! Try Again!");
+        }
+    }
+
+    public void BtnMatchMenu()
+    {
+        SceneManager.LoadScene("MatchMenu");
+    }
+
+    public void BtnGameSelect()
+    {
+        SceneManager.LoadScene("GameModeSelect");
+    }
+}
+
